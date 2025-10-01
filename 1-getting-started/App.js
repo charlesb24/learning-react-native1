db78e3fb7cee6f52a5552c6c8860f9cd4d4426e4
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-09-30 19:11:07 -0500

    add buttons and style react native components

--- 1-getting-started/App.js
+++ 1-getting-started/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View style={ styles.container }>
+
+      <Text style={ styles.styled }>
+        Hello World!
+      </Text>
+
+      <Button title="Tap me!"/>
+
+      <StatusBar style="auto"/>
+
     </View>
   );
 }
@@ -16,5 +23,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  styled: {
+    borderColor: '#000',
+    borderWidth: 2,
+    margin: 25,
+    padding: 10,
   },
 });
