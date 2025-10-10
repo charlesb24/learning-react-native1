3312dc16d009d785e0d161eb7a0f2b19d146e450
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-10-10 17:10:30 -0500

    improve layout of first screen and add styling to custom buttons

--- 2-components-deep-dive/components/PrimaryButton.js
+++ 2-components-deep-dive/components/PrimaryButton.js
@@ -1,9 +1,46 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 export default function PrimaryButton({ children }) {
+
+  function handlePress() {
+    console.log(`pressed: ${children}`)
+  }
+
   return (
-    <View>
-      <Text>{ children }</Text>
+    <View style={ styles.outerContainer }>
+      <Pressable
+        onPress={ handlePress }
+        android_ripple={{ color: '#640233' }}
+        style={ ({ pressed }) =>
+          pressed
+            ? [ styles.pressed, styles.innerContainer ]
+            : styles.innerContainer
+        }
+      >
+        <Text style={ styles.text }>{ children }</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  innerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- 2-components-deep-dive/screens/StartGameScreen.js
+++ 2-components-deep-dive/screens/StartGameScreen.js
@@ -12,9 +12,13 @@ export default function StartGameScreen() {
         autoCapitalize="none"
         autoCorrect={ false }
       />
-      <View>
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+      <View style={ styles.buttonsContainer }>
+        <View style={ styles.buttonContainer }>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={ styles.buttonContainer }>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
       </View>
     </View>
   );
@@ -22,10 +26,12 @@ export default function StartGameScreen() {
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#72063c',
+    backgroundColor: '#4e0329',
     borderRadius: 8,
     elevation: 4,
     shadowColor: 'black',
@@ -43,5 +49,11 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
