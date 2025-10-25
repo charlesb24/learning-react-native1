4093dd04826730e4042b5462e0487bf7e805f9f6
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-10-24 19:34:21 -0500

    use drawer navigation, style drawer screens with icons and colors, and use bottom tabs with styling and icons

---
+++ 4-navigation/3-bottom-tabs/App.js
@@ -0,0 +1,37 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
+
+const BottomTab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <BottomTab.Navigator id="main" screenOptions={{
+        headerStyle: { backgroundColor: '#3c0a6b' },
+        headerTintColor: 'white',
+        tabBarActiveTintColor: '#3c0a6b',
+        tabBarActiveBackgroundColor: '#f0e1ff',
+      }}>
+        <BottomTab.Screen
+          name="Welcome"
+          component={ WelcomeScreen }
+          options={{
+            tabBarLabel: 'Welcome Screen',
+            tabBarIcon: ({ color, size }) => <Ionicons name="home" color={ color } size={ size } />
+          }}
+        />
+        <BottomTab.Screen
+          name="User"
+          component={ UserScreen }
+          options={{
+            tabBarIcon: ({ color, size }) => <Ionicons name="person" color={ color } size={ size } />
+          }}
+        />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  );
+}
